04b022a6453efe819765ed2a50017813ca6cf477
Jakub Wąsowicz-Kiryło <jakub.wasowicz-kirylo@troteclaser.com>
2025-02-10 16:51:46 +0100

    More configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+// config/plugins.js
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "azure-storage",
+      providerOptions: {
+        account: env("STORAGE_ACCOUNT"),
+        accountKey: env("STORAGE_ACCOUNT_KEY"),
+        serviceBaseURL: env("STORAGE_URL"),
+        containerName: env("STORAGE_ACCOUNT_CONTAINER"),
+        defaultPath: "uploads",
+        cdnBaseURL: env("CDN_BASE_URL"),
+      },
+    },
+  },
+});
